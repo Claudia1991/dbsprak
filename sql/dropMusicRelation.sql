ec70f4f44fe5b4e6ba63b08fa863af96ca98e53e
ZamiZez <ZamiZez@eaac5c0c-0029-11df-bce7-934f6fdf4811>
2010-01-16 13:38:24 +0000

    sql statements hinzugefügt

--- sql/dropMusicRelation.sql
+++ sql/dropMusicRelation.sql
@@ -13,5 +13,14 @@ CONNECT TO DBPrak;
 -- Aufgabe 1.3 -----------------------------------------------------
 -- Loeschen aller in 1.1 und 1.2 erzeugten Objekte -----------------
 --------------------------------------------------------------------
-
+DROP TRIGGER updatePrice;
+ALTER TABLE CDs DROP CONSTRAINT minNew;
+ALTER TABLE CDs DROP CONSTRAINT minUsed;
+ALTER TABLE Tracks DROP CONSTRAINT minDisc;
+ALTER TABLE Tracks DROP CONSTRAINT minTrack;
+ALTER TABLE Tracks DROP CONSTRAINT fkey;
+ALTER TABLE Tracks DROP CONSTRAINT pkey;
+ALTER TABLE CDs DROP CONSTRAINT pkey;
+DROP TABLE Tracks;
+DROP TABLE CDs;
 CONNECT RESET;
