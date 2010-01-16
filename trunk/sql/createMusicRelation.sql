bfeeaa4b88bde5dbfef1948693d52d5b9a5b9744
ZamiZez <ZamiZez@eaac5c0c-0029-11df-bce7-934f6fdf4811>
2010-01-16 13:38:24 +0000

    sql statements hinzugefügt

--- trunk/sql/createMusicRelation.sql
+++ trunk/sql/createMusicRelation.sql
@@ -13,18 +13,36 @@ CONNECT TO DBPrak;
 -- Aufgabe 1.1 -----------------------------------------------------
 -- Erzeugen der Tabellen zum Speichern der Musik Daten -------------
 --------------------------------------------------------------------
-
+CREATE TABLE CDs(
+  ASIN              CHAR(10),
+  Artist            VARCHAR(25),
+  Title             VARCHAR(50),
+  Label             VARCHAR(25),
+  NumberOfDiscs     INT,
+  LowestNewPrice    NUMERIC(5,2),
+  LowestUsedPrice   NUMERIC(5,2)
+);
+CREATE TABLE Tracks(
+  ASIN              CHAR(10),
+  DiscNumber        INT,
+  TrackNumber       INT,
+  Title             VARCHAR(50)
+);
 -- Aufgabe 1.2 -----------------------------------------------------
 -- Erstellen von Primaer-/Fremdschluesseln, Contraints, Triggern ---
 --------------------------------------------------------------------
 -- Primarykeys
-
+ALTER TABLE CDs ADD CONSTRAINT pkey PRIMARY KEY(ASIN);
+ALTER TABLE Tracks ADD CONSTRAINT pkey PRIMARY KEY(ASIN, DiscNumber, TrackNumber);
 -- Foreignkeys
-
+ALTER TABLE Tracks ADD CONSTRAINT fkey FOREIGN KEY(ASIN) REFERENCES CDs(ASIN);
 -- Constraints
-
+ALTER TABLE Tracks ADD CONSTRAINT minDisc CHECK DiscNumber > 0;
+ALTER TABLE Tracks ADD CONSTRAINT minTrack CHECK TrackNumber > 0;
+ALTER TABLE CDs ADD CONSTRAINT minNew CHECK LowestNewPrice >= 0;
+ALTER TABLE CDs ADD CONSTRAINT minUsed CHECK LowestUsedPrice >= 0;
 -- Trigger
-
+CREATE TRIGGER updatePrice BEFORE UPDATE OF LowestNewPrice ON CDs REFERENCING NEW AS newtab, OLD AS oldtab FOR EACH ROW UPDATE CDs SET newtab.LowestUsedPrice=oldtab.LowestUsedPrice*(newtab.LowestNewPrice/oldtab.LowestUsedPrice)
 -- evtl. Views
 
 CONNECT RESET;
