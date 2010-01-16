ef49bd5025c4628d1830ac937e1583681a7fcaea
ZamiZez <ZamiZez@eaac5c0c-0029-11df-bce7-934f6fdf4811>
2010-01-16 14:05:54 +0000

    sql update

--- trunk/sql/createMusicRelation.sql
+++ trunk/sql/createMusicRelation.sql
@@ -14,7 +14,7 @@ CONNECT TO DBPrak;
 -- Erzeugen der Tabellen zum Speichern der Musik Daten -------------
 --------------------------------------------------------------------
 CREATE TABLE CDs(
-  ASIN              CHAR(10),
+  ASIN              CHAR(10)        NOT NULL,
   Artist            VARCHAR(25),
   Title             VARCHAR(50),
   Label             VARCHAR(25),
@@ -23,9 +23,9 @@ CREATE TABLE CDs(
   LowestUsedPrice   NUMERIC(5,2)
 );
 CREATE TABLE Tracks(
-  ASIN              CHAR(10),
-  DiscNumber        INT,
-  TrackNumber       INT,
+  ASIN              CHAR(10)        NOT NULL,
+  DiscNumber        INT             NOT NULL,
+  TrackNumber       INT             NOT NULL,
   Title             VARCHAR(50)
 );
 -- Aufgabe 1.2 -----------------------------------------------------
@@ -37,12 +37,12 @@ ALTER TABLE Tracks ADD CONSTRAINT pkey PRIMARY KEY(ASIN, DiscNumber, TrackNumber
 -- Foreignkeys
 ALTER TABLE Tracks ADD CONSTRAINT fkey FOREIGN KEY(ASIN) REFERENCES CDs(ASIN);
 -- Constraints
-ALTER TABLE Tracks ADD CONSTRAINT minDisc CHECK DiscNumber > 0;
-ALTER TABLE Tracks ADD CONSTRAINT minTrack CHECK TrackNumber > 0;
-ALTER TABLE CDs ADD CONSTRAINT minNew CHECK LowestNewPrice >= 0;
-ALTER TABLE CDs ADD CONSTRAINT minUsed CHECK LowestUsedPrice >= 0;
+ALTER TABLE Tracks ADD CONSTRAINT minDisc CHECK (DiscNumber > 0);
+ALTER TABLE Tracks ADD CONSTRAINT minTrack CHECK (TrackNumber > 0);
+ALTER TABLE CDs ADD CONSTRAINT minNew CHECK (LowestNewPrice >= 0);
+ALTER TABLE CDs ADD CONSTRAINT minUsed CHECK (LowestUsedPrice >= 0);
 -- Trigger
-CREATE TRIGGER updatePrice BEFORE UPDATE OF LowestNewPrice ON CDs REFERENCING NEW AS newtab, OLD AS oldtab FOR EACH ROW UPDATE CDs SET newtab.LowestUsedPrice=oldtab.LowestUsedPrice*(newtab.LowestNewPrice/oldtab.LowestUsedPrice);
+CREATE TRIGGER updatePrice BEFORE UPDATE OF LowestNewPrice ON CDs REFERENCING NEW AS newtab OLD AS oldtab FOR EACH ROW SET newtab.LowestUsedPrice = (oldtab.LowestUsedPrice * (newtab.LowestNewPrice / oldtab.LowestNewPrice));
 -- evtl. Views
 
 CONNECT RESET;
