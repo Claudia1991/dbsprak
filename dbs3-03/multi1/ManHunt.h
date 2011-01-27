80d22779d9267e148d276ccd8df6c6f78a3108d4
ZamiZez <ZamiZez@eaac5c0c-0029-11df-bce7-934f6fdf4811>
2011-01-27 23:29:54 +0000

    irgendwelche updates

--- dbs3-03/multi1/ManHunt.h
+++ dbs3-03/multi1/ManHunt.h
@@ -6,16 +6,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <sched.h>
+
 
 #include<tr1/unordered_map>
 
+//class Person {
+//public:
+//	int from,to;
+//	Person(int from,int to){
+//		this->from = from;
+//		this->to = to;
+//	}
+//};
+
 class Person {
 public:
-	int from,to;
-	Person(int from,int to){
-		this->from = from;
-		this->to = to;
-	}
+  int from,to,direction;
+  Person(int from, int to,int direction){
+    this->from = from;
+    this->to = to;
+    this->direction = direction;
+  }
 };
 
 typedef std::tr1::unordered_map<int, bool> HMap;
