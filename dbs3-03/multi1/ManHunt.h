954de3364ff8cdb25b38af7ab16679210e5435af
ZamiZez <ZamiZez@eaac5c0c-0029-11df-bce7-934f6fdf4811>
2011-01-28 00:54:38 +0000

    zander stinkt!

--- dbs3-03/multi1/ManHunt.h
+++ dbs3-03/multi1/ManHunt.h
@@ -6,19 +6,52 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <errno.h>
 #include <sched.h>
+#include <tr1/unordered_map>
 
+#include <time.h>
+#include <sys/time.h>
 
-#include<tr1/unordered_map>
+static inline long long int getRDTSC() {
+  long long int rc;
+  asm __volatile__("rdtsc\n\t"
+      "mov %%eax,(%%rsi)\n\t"
+      "mov %%edx,4(%%rsi)"
+      :: "S" (&rc));
+  return rc;
+}
 
-//class Person {
-//public:
-//	int from,to;
-//	Person(int from,int to){
-//		this->from = from;
-//		this->to = to;
-//	}
-//};
+static inline double getCPUFreq()
+{
+ struct timeval tpStart,tpStop;
+ gettimeofday(&tpStart,0);
+
+ long long int rStart,rStop;
+
+ rStart = getRDTSC();
+
+ sleep(2);
+
+ rStop = getRDTSC();
+
+ gettimeofday(&tpStop,0);
+
+ long long int ticks = rStop-rStart;
+
+ long tsec = tpStop.tv_sec - tpStart.tv_sec;
+ long tmsec = tpStop.tv_usec - tpStart.tv_usec;
+
+ long t = tsec * 1000000 + tmsec;
+
+ double freq = ((double) ticks / (double) t) *1000000.0;
+
+#ifdef _DEBUG
+ printf("msec : %d ticks: %lld freq: %g GHz\n",t,freq/1E9);
+#endif
+
+ return freq;
+}
 
 class Person {
 public:
